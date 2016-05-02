1247f7a00c2e8f34083432731273a36113ee5250
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-05-02 11:37:57 +0200

    Tests in same order as thesis

--- test.sh
+++ test.sh
@@ -1,10 +1,34 @@
 if [ -z "$1" ]; then
-  tests="$(echo *-hprolog.pl)"
+  tests="500fib
+	 750fib
+	 1000fib
+	 10kfib
+	 10krecognize
+	 20krecognize
+	 50krecognize
+	 500naiveReverse
+	 1000naiveReverse
+	 2000shuttle
+	 5000shuttle
+	 20000shuttle
+	 50000shuttle
+	 10kpingpong
+	 20kpingpong
+	 50pDblFstLoop
+	 100pDblFstLoop
+	 50pDoubleFirst
+	 100pDoubleFirst
+	 200pDoubleFirst
+	 500pDoubleFirst
+	 pyramid500
+	 bintree18
+	 testJoins
+	 mondial"
 else
-  tests="$*"
+  tests="$(echo "$*" | sed 's/-hprolog.pl//g')"
 fi
 
 for f in $tests; do
-  echo "TEST $f ..."
-  swipl -g go,halt -G8g $f
+# echo "TEST $f ..."
+  swipl -g go,halt -G8g $f-hprolog.pl
 done
