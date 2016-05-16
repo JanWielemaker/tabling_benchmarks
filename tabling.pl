63efed2e70e42bb058551599532cabc5c1fa562e
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-05-16 17:03:34 +0200

    Added hProlog timing

--- tabling.pl
+++ tabling.pl
@@ -33,3 +33,29 @@ verboseln(T) :-
 	getenv('VERBOSE', y), !,
 	writeln(T).
 verboseln(_).
+
+
+hprolog_time(24,     '500fib-hprolog.pl').
+hprolog_time(33,     '750fib-hprolog.pl').
+hprolog_time(46,     '1000fib-hprolog.pl').
+hprolog_time(982,    '10kfib-hprolog.pl').
+hprolog_time(205,    '20krecognize-hprolog.pl').
+hprolog_time(503,    '50krecognize-hprolog.pl').
+hprolog_time(767,    '500naiveReverse-hprolog.pl').
+hprolog_time(2800,   '1000naiveReverse-hprolog.pl').
+hprolog_time(44,     '2000shuttle-hprolog.pl').
+hprolog_time(138,    '5000shuttle-hprolog.pl').
+hprolog_time(582,    '20000shuttle-hprolog.pl').
+hprolog_time(1586,   '50000shuttle-hprolog.pl').
+hprolog_time(271,    '10kpingpong-hprolog.pl').
+hprolog_time(490,    '20kpingpong-hprolog.pl').
+hprolog_time(653,    '50pDblFstLoop-hprolog.pl').
+hprolog_time(4638,   '100pDblFstLoop-hprolog.pl').
+hprolog_time(162,    '50pDoubleFirst-hprolog.pl').
+hprolog_time(989,    '100pDoubleFirst-hprolog.pl').
+hprolog_time(6785,   '200pDoubleFirst-hprolog.pl').
+hprolog_time(110463, '500pDoubleFirst-hprolog.pl').
+hprolog_time(1914,   'pyramid500-hprolog.pl').
+hprolog_time(108662, 'bintree18-hprolog.pl').
+hprolog_time(3001,   'testJoins-hprolog.pl').
+hprolog_time(6444,   'mondial-hprolog.pl').
