ce41d49cc55243f182786f5b164455f611a8810d
Jan Wielemaker <j.wielemaker@cwi.nl>
2019-07-26 13:48:32 +0200

    Removed old emulation for fib750.

--- 750fib-swi.pl
+++ 750fib-swi.pl
@@ -6,15 +6,10 @@
 %
 % This is also interesting for a paper (Marko, Tom).
 
-% :-table fib/2.
-
-:- ['./tabling.pl'].
+:-table fib/2.
 
 entry(fib(750,_Fib)).
 
-fib(X,Y) :-
-  start_tabling(fib(X,Y),fib_aux(X,Y)).
-
-fib_aux(0, 1):-!.
-fib_aux(1, 1):-!.
-fib_aux(N,F):-N>1,N1 is N-1, N2 is N-2,fib(N1,F1),fib(N2,F2),F is F1+F2.
+fib(0, 1):-!.
+fib(1, 1):-!.
+fib(N,F):-N>1,N1 is N-1, N2 is N-2,fib(N1,F1),fib(N2,F2),F is F1+F2.
